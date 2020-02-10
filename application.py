4fdaff2adef751f287bb22706fabfa36f737c240
Majdi Muwanes <majdi.muwanes@gmail.com>
2020-02-09 20:12:16 -0800

    NA

--- application.py
+++ application.py
@@ -2,50 +2,21 @@
 import os
 
 from flask import Flask, session, render_template, url_for, redirect, request
-from flask_socketio import SocketIO, emit
+from flask_socketio import SocketIO, emit, send, join_room, leave_room
 from flask_session import Session
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
-users = ['guest'] #check dictionaries 
-chatrooms = ['public']
+user = []
+usersList = {} 
+channels = {}
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    if 'user' in session:
-        user = session['user']
-        return redirect(url_for('/chat'))
-    # if request.method == "POST":
-    #     user = request.form.get('username')
-    #     if users.count(user) == 0:
-    #         session['user'] = user
-    #         return redirect(url_for('chat'))
-    #     else:
-    #         message = 'username already exists, please choose a different username'
-    #         return render_template('index.html', message=message)
     return render_template('index.html')
 
-
-@socketio.on('user login') #why would you log anyone in via socket.io????? WHY?!?
-def enter(data): 
-    user = data['username']
-    users.append(user)
-    emit('user connected', user, broadcast=True)   
-
-
-@socketio.on('user connected')
-@app.route("/chat")
-def chat():
-    return render_template("chat.html", user=users)
-    
-# @app.route("/chat", methods=["GET","POST"])
-# def chat(user):
-#     if 'user' in session:
-#         return render_template('chat.html', user=user, users=users)
-#     else:
-#         return redirect(url_for('index'))
 
 @app.route("/logout", methods=["POST"])
 def logout():
--- static/index.js
+++ static/index.js
@@ -3,13 +3,11 @@ document.addEventListener('DOMContentLoaded', () => {
     //connect to websocket
     var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
 
-    //when connected, configure buttons to submit - Why are you doing this?????? 
-    socket.on('connect', () => {
-        document.querySelector("button").onclick = () => {
-            const username = document.getElementById('#username').value;
-            socket.emit('user login', { 'username': username });
-        };
-    });
 
+    socket.on('connect', () => { 
+          
+         });
+         
+    
 
 });

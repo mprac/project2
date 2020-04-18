ca2239e48beaca6d8ade585179b11ec2ef08bb73
Majdi Muwanes <majdi.muwanes@gmail.com>
2020-04-17 19:06:58 -0700

    ChannelCreation_JoinLeaveChannels
    create channel, update list, join and leave channel

--- application.py
+++ application.py
@@ -12,92 +12,118 @@ socketio = SocketIO(app)
 # {} set - a collection which is unordered and unindexed. no duplicates
 # {} dictionary - is a collection which is unordered changeable and indexed.no duplicates
 
-joinedUsers = {} #WHY THE USERNAME IS IN THE CHANNELS LIST
+joinedUsers = {}  # WHY IF THE USERNAME IS IN THE CHANNELS LIST - channel list holds channel name and messages not users info
 channels = {}  # use this to store the channel name to be added and removed in localstorage
-channels['general'] = [] 
-allChannels = ['general']  #list of all channels to
-#numUsers = 0 # show how many users joined flack
+channels['general'] = []
+allChannels = ['general']  # list of all channels to
+# numUsers = 0 # show how many users joined flack
+
 
 @app.route("/")
 def index():
     return render_template('index.html')
 
-# when website loads, show all messages. 
+# when website loads, show all messages.
 @socketio.on('connect')
 def broadcastdata():
-    emit('load messages', {'channels': channels})
+    emit('load data', {'channels': channels, 'allchannels': allChannels})
 
 # check if user is in joinedUsers list - OK.
 @socketio.on('user joined')
 def userjoined(data):
-    user = ""
-    error = ""
-    if data["person"] in joinedUsers:
+    user = data['person'].strip()
+    if user in joinedUsers:
         error = "User already exists please use a different name"
         emit('error', {'error': error})
+    elif not user:
+        error = "Username cannot be blank"
+        emit('error', {'error': error})
+    elif ' ' in user:
+        error = "Username cannot contain spaces"
+        emit('error', {'error': error})
     else:
-        userInfo = {'id': request.sid,'username': data['person']}
+        userInfo = {'id': request.sid, 'username': data['person']}
         joinedUsers[data['person']] = userInfo
-        user = data['person']
         emit('user validation', {'user': user})
 # END check if user is in joinedUsers list
 
-#broadcast data
-@socketio.on('update data')
-def updatedata():
-    emit('broadcast data', {'joinedUsers': joinedUsers}, broadcast=True)
+# broadcast data
+# @socketio.on('update data')
+# def updatedata():
+#     emit('broadcast data', {'joinedUsers': joinedUsers}, broadcast=True)
 
-
+# send message
 @socketio.on('send message')
 def message(data):
-    #add data to dictionary
+    # add data to dictionary
     message = {'message': data['message'],
-               'username': data['username'], 'date': data['date']} 
-    channels['general'].append(message) #adds message data to general 
-    if (len(channels['general'])>100):
-        channels['general'].pop(0)
-    emit('load messages', {'channels': channels}, broadcast=True)
+               'username': data['username'], 'date': data['date']}
+    channels[data['channel']].append(message)  # adds message data to general
+    if (len(channels[data['channel']]) > 100):
+        channels[data['channel']].pop(0)
+    emit('load data', {'channels': channels,
+                       'allchannels': allChannels}, broadcast=True)
+# END send message
 
-# creat channel 
+# create channel
 @socketio.on('create channel')
 def create_channel(data):
+    error = ""
     channel = (data['channel'].strip()).lower()
     if not channel:
-        error = "server error: channel cannot be empty"
+        error = "Server error: channel cannot be empty"
     elif channel[0].isdigit():
-        error = "server error: channel name cannot start with a number"
+        error = "Server error: channel name cannot start with a number"
     elif ' ' in channel:
-        error = "server error: channel name cannot contain spaces"
+        error = "Server error: channel name cannot contain spaces"
     elif channel in allChannels:
-        error = "server error: channel already exists"
+        error = "Server error: channel already exists"
     else:
         allChannels.append(channel)
-        channels[channel]=[]
-    emit('created channel', {'channel': channel, 'error': error})
+        channels[channel] = []
+    emit('created channel', {'channel': channel,
+                             'allchannels': allChannels, 'error': error})
+# END creat channel
 
-
+# join channel_room
 @socketio.on('join')
 def on_join(data):
     username = data['username']
     room = data['channel']
-    join_room(room)
-    send(username + ' has entered the room.', room=room)
+    message = {'message': data['message'],
+               'username': data['username'], 'date': data['date']}
+    #oin_room(room)
+    channels[data['channel']].append(message)  # adds message data to general
+    if (len(channels[data['channel']]) > 100):
+        channels[data['channel']].pop(0)
+    emit('load data', {'channels': channels,
+                       'allchannels': allChannels}, broadcast=True)
+    #send('load data', username + ' has entered the room.', room=room)
+# END join channel_room
 
+# leave channel_room
 @socketio.on('leave')
 def on_leave(data):
     username = data['username']
     room = data['channel']
-    leave_room(room)
-    send(username + ' has left the room.', room=room)
+    message = {'message': data['message'],
+               'username': data['username'], 'date': data['date']}
+    #leave_room(room)
+    channels[data['channel']].append(message)
+    if (len(channels[data['channel']]) > 100):
+        channels[data['channel']].pop(0)
+    emit('load data', {'channels': channels,
+                       'allchannels': allChannels}, broadcast=True)
+# END leave channel_room
 
-
+# exit chat
 @socketio.on('exit')
 def exit(data):
     user = data['person']
     joinedUsers.pop(user)
-    emit('broadcast data', {'joinedUsers': joinedUsers}, broadcast=True)
+    # emit('load data', {'joinedUsers': joinedUsers, 'allchannels': allChannels}, broadcast=True)
     return redirect(url_for('index'))
-   
+# END exit chat
 
 
 if __name__ == '__main__':
--- static/index.js
+++ static/index.js
@@ -3,65 +3,55 @@ document.addEventListener('DOMContentLoaded', () => {
     //connect to websocket
     var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
 
-    //joinedRoom=false;
-    //inChannel=false;
     socket.on('connect', () => {
-        //jQuery('#chat').html("");
-
         //Check if a username is in localstorage #####
         if (!localStorage.getItem("username")) {
             //show modal to request username
+            appHeading()
             jQuery('#joinChatModal').modal('toggle'); //$ is just an alias to jQuery
         } else {
-            $('#joinChatModal').modal('hide');
-            document.querySelector("#userinchannel").innerHTML = localStorage.getItem("username");
+            jQuery('#joinChatModal').modal('hide');
         }
-
     });
 
-    // Join Chat #####
+    // # Join Chat #####
     document.querySelector('#userJoin').onclick = () => {
         //get username and save to emit
-        var name = document.querySelector('#username').value;
-        var person = name.toLowerCase();
+        const name = document.querySelector('#username').value;
+        const person = name.toLowerCase();
         socket.emit('user joined', { 'person': person });
     };
-
     socket.on('user validation', data => {
         $('#joinChatModal').modal('hide');
         localStorage.setItem("username", data['user']);
-        document.querySelector("#userinchannel").innerHTML = localStorage.getItem("username");
-        socket.emit('update data')
+        localStorage.setItem('inChannel', 'general');
+        appHeading();
+        const username = localStorage.getItem('username');
+        const inChannel = localStorage.getItem('inChannel');
+        const date = new Date().toLocaleString();
+        socket.emit('join', { 'channel': inChannel, 'username': username, 'message': username + ' has joined FLACK ', 'date': date })
+        //socket.emit('update data')
     });
-
     //show username error
     socket.on('error', data => {
         document.querySelector('#joinChatModalLabel').innerHTML = data['error'];
+        jQuery('#joinChatModal').modal('show');
     });
 
-    //load broadcasted data
-    socket.on('broadcast data', data => {
-        jQuery('#joinedUsers').html("");
-        for (const x in data['joinedUsers']) {
-            document.querySelector('#joinedUsers').innerHTML = data['joinedUsers'][x]['username'];
-        }
-        
-    });
-    // END Join Chat #####
+    //###### END Join Chat #####
 
-    // Send message using Button #####
+    // # Send message #####
+    // Send message using Button
     document.querySelector('#sendMessageButton').onclick = () => {
         const message = document.querySelector("#messageChannelInput").value;
-        if (message.trim() == "") {
-            //do nothing
-        } else {
+        if (message.trim() !== "") {
             const message = document.querySelector("#messageChannelInput").value;
             const username = localStorage.getItem('username');
             const date = new Date().toLocaleString();
-            socket.emit('send message', { 'message': message, 'username': username, 'date': date });
+            const inChannel = localStorage.getItem('inChannel');
+            socket.emit('send message', { 'message': message, 'username': username, 'date': date, 'channel': inChannel });
             document.querySelector('#messageChannelInput').value = "";
-        }
-
+        };
     };
     // Send message using ENTER KEY
     document.addEventListener('keyup', function (key) {
@@ -70,73 +60,105 @@ document.addEventListener('DOMContentLoaded', () => {
             const message = document.querySelector("#messageChannelInput").value;
             const username = localStorage.getItem('username');
             const date = new Date().toLocaleString();
-            socket.emit('send message', { 'message': message, 'username': username, 'date': date });
+            const inChannel = localStorage.getItem('inChannel');
+            socket.emit('send message', { 'message': message, 'username': username, 'date': date, 'channel': inChannel });
             document.querySelector('#messageChannelInput').value = "";
         };
-
     });
-
     //update client side with message
-    socket.on('load messages', data => {
+    socket.on('load data', data => {
         jQuery('#messageChannelInput').focus();
         submitmessage(data);
+        updateChannelList(data);
+        appHeading();
     });
-    // END Send message  #####
+    // ####### END Send message  #####
 
-    //create channel #####
+    // # create channel #####
     document.querySelector('#createChannelButton').onclick = () => {
         const channel = document.querySelector('#messageChannelInput').value;
         if (channel.trim() == "" || (!isNaN(channel[0]))) {
             alert('Channel names must start with a letter and contain no spaces');
         } else {
+            localStorage.setItem('inChannel', channel);
             socket.emit('create channel', { 'channel': channel });
             document.querySelector('#messageChannelInput').value = "";
         };
     };
-
     socket.on('created channel', data => {
         if (data['error'] !== "") {
-            alert(data['error'])
+            alert(data['error']);
+            console.log('error');
         } else {
-            createChannel(data);
+            updateChannelList(data);
+            const username = localStorage.getItem('username');
+            const inChannel = localStorage.getItem('inChannel');
+            const date = new Date().toLocaleString();
+            socket.emit('join', { 'channel': inChannel, 'username': username, 'message': username + ' has created the channel ' + inChannel, 'date': date })
+            console.log('success');
         };
-
-
     });
-    // END create channel 
+    // ##### END create channel #####
 
-
-    //exit chat
+    // # exit chat #####
     document.querySelector('#exit').onclick = () => {
-        var person = localStorage.getItem('username');
+        //new
+        const username = localStorage.getItem('username');
+        const inChannel = localStorage.getItem('inChannel');
+        const date = new Date().toLocaleString();
+        socket.emit('leave', { 'channel': inChannel, 'username': username, 'message': username + ' has exited FLACK', 'date': date })
+        //end new 
+        const person = localStorage.getItem('username');
         socket.emit('exit', { 'person': person });
         localStorage.clear();
+        console.log('exit success');
     };
+    // ###### END exit chat #####
+
+    //
+
+    leaveChannel = function leaveChannel() {
+        const username = localStorage.getItem('username');
+        const inChannel = localStorage.getItem('inChannel');
+        const date = new Date().toLocaleString();
+        socket.emit('leave', { 'channel': inChannel, 'username': username, 'message': username + ' has left ' + inChannel, 'date': date })
+        localStorage.setItem('inChannel', this.id);
+        joinChannel();
+
+    };
+    joinChannel = function joinChannel() {
+        const username = localStorage.getItem('username');
+        const inChannel = localStorage.getItem('inChannel');
+        const date = new Date().toLocaleString();
+        socket.emit('join', { 'channel': inChannel, 'username': username, 'message': username + ' has joined ' + inChannel, 'date': date })
+    };
+
+    //
 
 });
 
-//###### UPDATE THIS TO ACCEPT MESSAGES BASED ON CHANNEL
-//function to submit message
+// # function to submit message #####
 function submitmessage(data) {
     jQuery('#chat').html("");
-    for (const x in data['channels']['general']) {
+    const inChannel = localStorage.getItem('inChannel');
+    for (const x in data['channels'][inChannel]) {
         const message = document.createElement('div');
-        if (data['channels']['general'][x]['username'] == localStorage.getItem('username')) {
+        if (data['channels'][inChannel][x]['username'] == localStorage.getItem('username')) {
             message.className = 'thisuser messages';
         } else {
             message.className = 'otheruser messages';
         }
         const messageValue = document.createElement('div');
         messageValue.className = 'message';
-        messageValue.innerHTML = data['channels']['general'][x]['message'];
+        messageValue.innerHTML = data['channels'][inChannel][x]['message'];
         const messageUserDate = document.createElement('div');
         messageUserDate.className = "usernameDate";
         const messageUser = document.createElement('span')
         messageUser.className = 'username';
-        messageUser.innerHTML = data['channels']['general'][x]['username'];
+        messageUser.innerHTML = data['channels'][inChannel][x]['username'];
         const messageDate = document.createElement('span');
         messageDate.className = 'date';
-        messageDate.innerHTML = data['channels']['general'][x]['date'];
+        messageDate.innerHTML = data['channels'][inChannel][x]['date'];
 
         jQuery('#chat').append(message);
         message.append(messageValue);
@@ -145,25 +167,53 @@ function submitmessage(data) {
         messageUserDate.append(messageDate);
         $(window).scrollTop(999999999);
     }
-}
 
-function createChannel(data) {
+};
+// ###### END function to submit message #####
+
+// # update channelList #####
+function updateChannelList(data) {
     jQuery('#channelList').html("");
-    channel = data['channel'];
-    localStorage.setItem('channel', channel);
-    username = localStorage.getItem('username');
+    const getChannel = localStorage.getItem('inChannel');
+    //const username = localStorage.getItem('username'); // to join channel
 
-    socket.emit('join', { 'channel': channel, 'username': username })
+    const listGroup = document.createElement('div');
+    listGroup.className = 'list-group';
+    // channels has list of all channels, channel is the channel that will be joined
+    for (achannel in data['allchannels']) {
+        const listItem = document.createElement('button');
+        listItem.type = 'button';
+        listItem.onclick = leaveChannel;
+        if (data['allchannels'][achannel] == getChannel) {
+            listItem.className = 'channellistitem list-group-item list-group-item-action active'
+            listItem.setAttribute('id', getChannel);
+            listItem.disabled = true;
+            listItem.innerHTML = getChannel;
+        } else {
+            listItem.className = 'channellistitem list-group-item list-group-item-action'
+            listItem.setAttribute('id', data['allchannels'][achannel]);
+            listItem.innerHTML = data['allchannels'][achannel];
+        }
 
-    // write what you will do
-    // 1. create elements
-    // 2. add active in class to know that channel is active
-    // 3. append channel to list
-    // 4. add channel to active variable 
-    // 5. have user that created channel join the room. 
-    // 6. must send new room and username to join_room. 
+        jQuery('#channelList').append(listGroup);
+        listGroup.append(listItem);
+    }
+    appHeading;
+
+};
+// ##### END update channelList #####
+
+// # update application heading #####
+function appHeading() {
+    if (!localStorage.getItem('username')) {
+        document.querySelector("#userinchannel").innerHTML = 'FLACK'
+    } else {
+        document.querySelector("#userinchannel").innerHTML = localStorage.getItem("username") + ' in ' + localStorage.getItem('inChannel');
+    }
 
 }
+// ##### update application heading #####
+
 
 
 
